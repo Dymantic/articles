f9f41c3b8179d923693c55cf24abb892cf836b64
Michael Joyner <joyner.michael@gmail.com>
2017-09-05 14:33:30 +0800

    add boot method to service provider

--- src/ArticlesServiceProvider.php
+++ src/ArticlesServiceProvider.php
@@ -8,8 +8,18 @@ use Illuminate\Support\ServiceProvider;
 
 class ArticlesServiceProvider extends ServiceProvider
 {
+
+    public function boot()
+    {
+        if (! class_exists('CreateArticlesTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_articles_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_articles_table.php'),
+            ], 'migrations');
+        }
+    }
+
     public function register()
     {
-
+        
     }
 }
